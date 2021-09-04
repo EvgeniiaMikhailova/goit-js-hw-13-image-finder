27b2fca4ad22de9545ad623492bf17317309d373
EvgeniiaMikhailova <evgeva99@gmail.com>
2021-09-04 23:35:18 +0300

    Добавляет скрипт

---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export const refs = {
+    galleryContainer: document.querySelector('.gallery'),
+    input: document.querySelector('.input-field'),
+    searchForm: document.querySelector('.search-form'),
+    loadMoreBtn: document.querySelector('.load-more-btn')
+}
